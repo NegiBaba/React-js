56c3a1c5c41c357ec3019d11f4f8879c2f531a17
Piyush Negi <48850319+NegiBaba@users.noreply.github.com>
2021-06-17 22:47:05 +0530

    First commit

---
+++ src/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Momo</li>
+      <li>Chicken</li>
+      <li>Chawal</li>
+    </ul>
+  );
+}
+
+export default List;
